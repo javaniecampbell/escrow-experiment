658362a79abcd602d70f9dc98640b08e74606ed6
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 01:46:35 -0500

    feat: Added logic  to create milestones

--- services/payment-service/routes/milestone.js
+++ services/payment-service/routes/milestone.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-
+const prisma = require('../utils/prisma');
 
 router.post('/add-milestone', async (req, res) => {
     const { projectId, milestoneName, amount } = req.body;
-    // Logic to add milestone to a project
-    res.status(201).send({ message: 'Milestone added' });
+    try {
+    
+        // Create a new milestone associated with the project
+        const milestone = await prisma.milestone.create({
+          data: {
+            milestoneName,
+            amount,
+            projectId,
+          },
+        });
+    
+        res.status(201).json(milestone);
+      } catch (error) {
+        console.error('Error adding milestone:', error);
+        res.status(500).json({ error: 'Unable to add milestone' });
+      }
+    // res.status(201).send({ message: 'Milestone added' });
 });
 
 module.exports = router;
