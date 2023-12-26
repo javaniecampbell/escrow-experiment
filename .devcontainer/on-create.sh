5b8244ecb5c6f507800c2be8100e2b0e4dd8e4b5
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 00:16:11 +0000

    feat: Update package.json and add new files for notification service

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install the rad cli
+CURRENT_BRANCH=$(git branch --show-current)
+
+if [ "$CURRENT_BRANCH" == "edge" ]; then
+    echo "Setting RADIUS_VERSION to edge"
+    RADIUS_VERSION=edge
+else
+    ## If CURRRENT_BRANCH matches a regex of the form "v0.20", set RADIUS_VERSION to the matching string minus the "v"
+    if [[ $CURRENT_BRANCH =~ ^v[0-9]+\.[0-9]+$ ]]; then
+        echo "Setting RADIUS_VERSION to $CURRENT_BRANCH"
+        RADIUS_VERSION=${CURRENT_BRANCH:1}
+    else
+        echo "Setting RADIUS_VERSION to edge"
+        RADIUS_VERSION=edge
+    fi
+fi
+
+wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
+
+## Upgrade to the latest version of the dotnet sdk 
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --version latest
---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Create a k3d cluster
+while (! kubectl cluster-info); do
+# Docker is not ready yet, wait a bit and try again
+    echo "Waiting for docker to be ready..."
+    k3d cluster delete
+    k3d cluster create -p '8081:80@loadbalancer' --k3s-arg '--disable=traefik@server:0'
+    sleep 1
+done
+
+## Install anything else you need here
+wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+dapr uninstall # clean up any previous installs
+dapr init
