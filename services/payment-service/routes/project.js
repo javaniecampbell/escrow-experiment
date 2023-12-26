0ea0ae0828764ae54a3502f2192c642cd5169906
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 01:44:28 -0500

    feat: added project creation functionality using Prisma

--- services/payment-service/routes/project.js
+++ services/payment-service/routes/project.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const prisma = require('../utils/prisma');
 
 router.post('/create-project', async (req, res) => {
-    const { projectName, description, milestones } = req.body;
-    // Logic to insert project into the database
+    // const { projectName, description, milestones } = req.body;
+    try {
+        const { projectName, description } = req.body;
+    
+        // Create a new project in the database
+        const project = await prisma.project.create({
+          data: {
+            projectName,
+            description,
+
+          },
+        });
+    
+        res.status(201).json(project);
+      } catch (error) {
+        console.error('Error creating project:', error);
+        res.status(500).json({ error: 'Unable to create project' });
+      }
     res.status(201).send({ message: 'Project created' });
 });
 
