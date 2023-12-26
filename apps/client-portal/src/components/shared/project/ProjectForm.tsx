041e157d3631a9ce5e43ff2e703280b2467265c4
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 02:06:37 -0500

    feat: Added ProjectForm component

---
+++ apps/client-portal/src/components/shared/project/ProjectForm.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ProjectForm() {
+  return (
+    <div>
+      {" "}
+      <h1>Create a New Project</h1>
+      <form
+        id="createProjectForm"
+        action="http://localhost:3000/api/create-project"
+        method="POST"
+      >
+        <label htmlFor="projectName">Project Name:</label>
+        <input type="text" id="projectName" required />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" required></textarea>
+
+        <button type="submit">Create Project</button>
+      </form>
+    </div>
+  );
+}
+
+export default ProjectForm;
