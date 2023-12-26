f57875e4cccae8a1610116f5c8875a8b390ae1fd
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 01:38:21 -0500

    refactor: added database schema and migrations

--- .scripts/migrations/V1__Initial_Schema.sql
+++ .scripts/migrations/V1__Initial_Schema.sql
@@ -1,27 +1,25 @@
-CREATE TABLE Projects
-(
+-- CREATE DATABASE escrowservice;
+-- USE EscrowService;
+-- GO
+CREATE TABLE Projects (
     ProjectID INT PRIMARY KEY,
     ProjectName VARCHAR(255),
     TotalAmount DECIMAL(10, 2)
 );
 
-CREATE TABLE Milestones
-(
+CREATE TABLE Milestones (
     MilestoneID INT PRIMARY KEY,
     ProjectID INT,
     MilestoneName VARCHAR(255),
     Amount DECIMAL(10, 2),
-    Status VARCHAR(50),
-    -- e.g., 'pending', 'completed'
+    Status VARCHAR(50), -- e.g., 'pending', 'completed'
     FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID)
 );
 
-CREATE TABLE DigitalAssets
-(
+CREATE TABLE DigitalAssets (
     AssetID INT PRIMARY KEY,
     MilestoneID INT,
-    AssetURL VARCHAR(255),
-    -- URL to the Azure Blob Storage resource
-    ExpiryDate DATETIME,
+    AssetURL VARCHAR(255), -- URL to the Azure Blob Storage resource
+    ExpiryDate TIMESTAMP,
     FOREIGN KEY (MilestoneID) REFERENCES Milestones(MilestoneID)
 );
