fda37dae26b3a222470b8f85f4e5744f49c52bfb
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-25 23:50:15 -0500

    feat; added the initial database schema

---
+++ .scripts/migrations/V1__Initial_Schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Projects
+(
+    ProjectID INT PRIMARY KEY,
+    ProjectName VARCHAR(255),
+    TotalAmount DECIMAL(10, 2)
+);
+
+CREATE TABLE Milestones
+(
+    MilestoneID INT PRIMARY KEY,
+    ProjectID INT,
+    MilestoneName VARCHAR(255),
+    Amount DECIMAL(10, 2),
+    Status VARCHAR(50),
+    -- e.g., 'pending', 'completed'
+    FOREIGN KEY (ProjectID) REFERENCES Projects(ProjectID)
+);
+
+CREATE TABLE DigitalAssets
+(
+    AssetID INT PRIMARY KEY,
+    MilestoneID INT,
+    AssetURL VARCHAR(255),
+    -- URL to the Azure Blob Storage resource
+    ExpiryDate DATETIME,
+    FOREIGN KEY (MilestoneID) REFERENCES Milestones(MilestoneID)
+);
