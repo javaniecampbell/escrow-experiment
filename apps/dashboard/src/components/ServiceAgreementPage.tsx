b06f5761d4202128d9c991fb0df928d088299791
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 03:37:25 +0000

    feat: filled out the service agreement page and styles

---
+++ apps/dashboard/src/components/ServiceAgreementPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ServiceAgreementPage() {
+  return (
+    <form action="/create-checkout-session" method="POST">
+    <input type="checkbox" id="agree" name="agree" required />
+    <label htmlFor="agree">I agree to the Terms and Conditions</label>
+    <button type="submit" className="bg-blue-500 text-white p-2">Proceed to Payment</button>
+</form>
+
+  )
+}
+
+export default ServiceAgreementPage
---
+++ apps/dashboard/src/pages/service-agreement.tsx
@@ -0,0 +1,12 @@
+import ServiceAgreementPage from '@/components/ServiceAgreementPage'
+import React from 'react'
+
+function ServiceAgreement() {
+  return (
+    <div>
+        <ServiceAgreementPage />
+    </div>
+  )
+}
+
+export default ServiceAgreement
