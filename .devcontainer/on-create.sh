597da53eb46041b8748ff9a0e8311fdaea38052a
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-26 01:03:02 +0000

    chore: Updated .devcontainer/on-create.sh script

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -17,9 +17,10 @@ else
     fi
 fi
 
-wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
-
 ## Upgrade to the latest version of the dotnet sdk 
 wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
 chmod +x ./dotnet-install.sh
 ./dotnet-install.sh --version latest
+
+wget -q "https://raw.githubusercontent.com/radius-project/radius/main/deploy/install.sh" -O - | /bin/bash
+
