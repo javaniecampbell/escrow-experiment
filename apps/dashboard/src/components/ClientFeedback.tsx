216f9ebf72197c5a276cfc77d43979531a683c86
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-27 19:26:18 +0000

    feat: created ClientFeedback component

---
+++ apps/dashboard/src/components/ClientFeedback.tsx
@@ -0,0 +1,31 @@
+// ClientFeedback.js
+import React, { useState } from 'react';
+
+const ClientFeedback = () => {
+  const [feedback, setFeedback] = useState('');
+
+  const handleSubmitFeedback = () => {
+    // Implement submitting feedback functionality here
+    // You can use state and form inputs to submit the feedback
+  };
+
+  return (
+    <div className="p-4 border border-gray-300 rounded">
+      <h2 className="text-lg font-semibold mb-4">Provide Feedback</h2>
+      <textarea
+        placeholder="Type your feedback here..."
+        value={feedback}
+        onChange={(e) => setFeedback(e.target.value)}
+        className="border border-gray-300 p-2 rounded w-full h-32"
+      />
+      <button
+        className="px-4 py-1 rounded bg-blue-500 text-white hover:bg-blue-600 mt-2"
+        onClick={handleSubmitFeedback}
+      >
+        Submit Feedback
+      </button>
+    </div>
+  );
+};
+
+export default ClientFeedback;
