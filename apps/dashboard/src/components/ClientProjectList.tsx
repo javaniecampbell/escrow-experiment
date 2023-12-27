5d52e5d3f7098ce41f960b32c9c2e5d5564bf05c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-27 18:00:14 +0000

    feat: Added ClientProjectList component

---
+++ apps/dashboard/src/components/ClientProjectList.tsx
@@ -0,0 +1,28 @@
+// ClientProjectList.js
+import React from 'react';
+import useStore from '@/shared/store';
+
+const ClientProjectList = () => {
+  const { clientProjects } = useStore();
+
+  return (
+    <div className="p-4 border border-gray-300 rounded">
+      <h2 className="text-lg font-semibold mb-4">Your Projects</h2>
+      {clientProjects.length > 0 ? (
+        <ul className="list-disc pl-6">
+          {clientProjects.map((project) => (
+            <li key={project.id} className="mb-3">
+              <h3 className="text-md font-semibold">{project.name}</h3>
+              <p>Status: {project.status}</p>
+              <p>Balance: ${project.balance.toFixed(2)}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-gray-700">You don't have any active projects.</p>
+      )}
+    </div>
+  );
+};
+
+export default ClientProjectList;
