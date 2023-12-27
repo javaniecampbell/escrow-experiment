54af45a101529f1eceea2280c6bec3014739ae47
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-27 19:04:24 +0000

    feat: created ClientProjects component

---
+++ apps/dashboard/src/components/ClientProjects.tsx
@@ -0,0 +1,46 @@
+// ClientProjects.js
+import React, { useState } from 'react';
+
+const ClientProjects = () => {
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      name: 'Website Redesign',
+      status: 'In Progress',
+    },
+    {
+      id: 2,
+      name: 'Mobile App Development',
+      status: 'Completed',
+    },
+    {
+      id: 3,
+      name: 'E-commerce Platform',
+      status: 'In Progress',
+    },
+  ]);
+
+  return (
+    <div className="p-4 border border-gray-300 rounded">
+      <h2 className="text-lg font-semibold mb-4">Projects</h2>
+      <table className="w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 p-2">Project Name</th>
+            <th className="border border-gray-300 p-2">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => (
+            <tr key={project.id}>
+              <td className="border border-gray-300 p-2">{project.name}</td>
+              <td className="border border-gray-300 p-2">{project.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ClientProjects;
