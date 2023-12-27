bbbf34b8773d3e707229821f6870d5f4966824f0
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-27 19:35:06 +0000

    feat: created ClientSupportFormV2 component

---
+++ apps/dashboard/src/components/ClientSupportFormV2.tsx
@@ -0,0 +1,33 @@
+// ClientSupport.js
+import React, { useState } from 'react';
+
+const ClientSupportFormV2 = () => {
+  const [message, setMessage] = useState('');
+
+  const handleSendMessage = () => {
+    // Implement sending the message to support
+    // You can use an API call to send messages to support
+    // Reset the message input field after sending
+    setMessage('');
+  };
+
+  return (
+    <div className="p-4 border border-gray-300 rounded">
+      <h2 className="text-lg font-semibold mb-4">Contact Support</h2>
+      <textarea
+        className="w-full p-2 border border-gray-300 rounded mb-2"
+        placeholder="Type your message here..."
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button
+        className="px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-600"
+        onClick={handleSendMessage}
+      >
+        Send Message
+      </button>
+    </div>
+  );
+};
+
+export default ClientSupportFormV2;
