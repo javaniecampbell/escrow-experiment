7ce3bdcc1337d18d50205a374c2dee38faf74eea
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2023-12-27 17:57:24 +0000

    feat: added Link to Service Agreement page

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -7,7 +8,8 @@ export default function Home() {
         <h1 className="text-xl font-bold">Welcome to Our Escrow Service</h1>
         <p>Here's how our service works...</p>
         {/* <!-- Add more content here --> */}
- 
+        <Link className="text-xl font-bold" href="/service-agreement">Let's get started</Link>
+        
     </main>
   )
 }
