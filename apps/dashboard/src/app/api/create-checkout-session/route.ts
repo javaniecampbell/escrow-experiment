186d814a8cfa08e317249eb55dd8e446ba707f9c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-01-03 21:50:32 +0000

    feat: updated Docker Compose and add create-checkout-session route

---
+++ apps/dashboard/src/app/api/create-checkout-session/route.ts
@@ -0,0 +1,13 @@
+export const dynamic = 'force-dynamic';
+
+export async function POST(req: Request) {
+    const res = await fetch('http://payment-service/api/payments/create-checkout-session', { body: JSON.stringify({ "amount": 3.00, "projectId": 1 }), method: 'POST' });
+    
+    let data;
+    if(res.ok){
+        data = await res.json();
+    }
+    data = await res.json();
+    
+    return Response.redirect(data.url);
+}
