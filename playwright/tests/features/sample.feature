2387b73e5e575ceec6c39d666becbd94020989c5
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-01-03 20:55:21 +0000

    feat: Added Google search functionality to sample.feature

--- playwright/tests/features/sample.feature
+++ playwright/tests/features/sample.feature
@@ -2,6 +2,6 @@ Feature: Is it Friday yet?
   Everybody wants to know when it's Friday
 
   Scenario: Sunday isn't Friday
-    Given today is Sunday
-    When I ask whether it's Friday yet
-    Then I should be told "Nope"
+    Given I am on the Google search page
+    When I search for "Is it Friday yet?"
+    Then I should see "" in the title
--- playwright/tests/steps-definitions/sample.step.ts
+++ playwright/tests/steps-definitions/sample.step.ts
@@ -1,3 +1,16 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { ICustomWorld } from "./custom-world";
 import { test, expect } from '@playwright/test';
+
+Given("I am on the Google search page", async function (this: ICustomWorld) {
+    await this.page?.goto("https://google.com");
+});
+
+When("I search for {string}", async function (this: ICustomWorld, text: string) {
+    await this.page?.fill('input[name="q"]', text);
+    await this.page?.press('input[name="q"]', "Enter");
+});
+
+Then("I should see {string} in the title", async function (this: ICustomWorld, text: string) {
+    expect(await this.page?.title()).toContain(text);
+});
