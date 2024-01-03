d2e26605dd1cf3cf63d71a34751e15a7b29e8ba3
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-01-03 20:29:48 +0000

    feat: configured Playwright test framework and sample feature file

---
+++ playwright/tests/steps-definitions/custom-world.ts
@@ -0,0 +1,18 @@
+import { IWorldOptions, World, setWorldConstructor } from '@cucumber/cucumber'
+import { BrowserContext, Page } from "@playwright/test";
+
+export interface ICustomWorld extends World {
+    context?: BrowserContext;
+    page?: Page;
+}
+
+export class CustomWorld extends World implements ICustomWorld {
+    public context?: BrowserContext;
+    public page?: Page;
+
+    constructor(options: IWorldOptions) {
+        super(options);
+    }
+}
+
+setWorldConstructor(CustomWorld);
---
+++ playwright/tests/steps-definitions/sample.step.ts
@@ -0,0 +1,3 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import { ICustomWorld } from "./custom-world";
+import { test, expect } from '@playwright/test';
