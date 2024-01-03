43af33bafd9ae9562bc81769d2a789ed53e366a7
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-01-03 21:52:18 +0000

    fix: redirect issue in create-checkout-session route

--- apps/dashboard/src/app/api/create-checkout-session/route.ts
+++ apps/dashboard/src/app/api/create-checkout-session/route.ts
@@ -2,12 +2,11 @@ export const dynamic = 'force-dynamic';
 
 export async function POST(req: Request) {
     const res = await fetch('http://payment-service/api/payments/create-checkout-session', { body: JSON.stringify({ "amount": 3.00, "projectId": 1 }), method: 'POST' });
-    
+
     let data;
-    if(res.ok){
+    if (res.ok) {
         data = await res.json();
+        return Response.redirect(data.url);
     }
-    data = await res.json();
-    
-    return Response.redirect(data.url);
+    Response.json({ error: 'Error creating checkout session' });
 }
