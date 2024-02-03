7830192a0b5675310ca46b31b4a764dcd9e81fe2
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 08:24:58 -0500

    feat: Added ClientEditProfile component to ClientProfilePage

--- apps/dashboard/src/pages/client/profile.tsx
+++ apps/dashboard/src/pages/client/profile.tsx
@@ -1,10 +1,12 @@
 import ClientProfile from "@/components/ClientProfile";
+import ClientEditProfile from "@/components/ClientEditProfile";
 import React from "react";
 
 function ClientProfilePage() {
   return (
     <div>
       <ClientProfile />
+      <ClientEditProfile />
     </div>
   );
 }
