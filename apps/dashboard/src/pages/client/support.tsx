3fdb1e61f1555f43e531bda020152b053e505dff
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 00:33:43 -0500

    feat: Added client support page

---
+++ apps/dashboard/src/pages/client/support.tsx
@@ -0,0 +1,14 @@
+import ClientSupport from "@/components/ClientSupport";
+import ClientSupportForm from "@/components/ClientSupportForm";
+import ClientSupportFormV2 from "@/components/ClientSupportFormV2";
+import React from "react";
+
+function ClientSupportPage() {
+  return (
+    <div>
+      <ClientSupport />
+    </div>
+  );
+}
+
+export default ClientSupportPage;
