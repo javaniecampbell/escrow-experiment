c053a7a9b2ec68a4e359ce6a9fac0f177a997f76
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 09:51:35 -0500

    feat: Added Project Feedback Page

---
+++ apps/dashboard/src/pages/client/feedback/[projectId].tsx
@@ -0,0 +1,13 @@
+import ClientFeedback from "@/components/ClientFeedback";
+import React from "react";
+
+function ProjectFeedbackPage() {
+  return (
+    <div>
+      <h1 className="">Project Feedback Page</h1>
+      <ClientFeedback />
+    </div>
+  );
+}
+
+export default ProjectFeedbackPage;
