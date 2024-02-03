7733ea5cc2cc32a749e402ede78222570a61d488
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 00:33:32 -0500

    feat: Added client profile page

---
+++ apps/dashboard/src/pages/client/profile.tsx
@@ -0,0 +1,12 @@
+import ClientProfile from "@/components/ClientProfile";
+import React from "react";
+
+function ClientProfilePage() {
+  return (
+    <div>
+      <ClientProfile />
+    </div>
+  );
+}
+
+export default ClientProfilePage;
