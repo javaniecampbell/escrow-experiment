bb45e2c703e93ed4292d7c1fbf05c9ce94d5b70d
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-02 23:29:31 -0500

    feat: Added ProjectList component to Dashboard page

--- apps/dashboard/src/pages/dashboard.tsx
+++ apps/dashboard/src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import DeliveredMilestones from "@/components/DeliveredMilestones";
 import MilestonesWithPreviews from "@/components/MilestonesWithPreviews";
 import MilestonesWithUpcomingPayouts from "@/components/MilestonesWithUpcomingPayouts";
+import ProjectList from "@/components/ProjectList";
 import ProjectOverview from "@/components/ProjectOverview";
 import TotalEscrowed from "@/components/TotalEscrowed";
 import TotalPayouts from "@/components/TotalPayouts";
@@ -8,13 +9,16 @@ import React from "react";
 
 function Dashboard() {
   return (
-    <div className="grid grid-cols-3 gap-1  p-4">
-      <ProjectOverview />
-      <TotalEscrowed />
-      <TotalPayouts />
-      <DeliveredMilestones />
-      <MilestonesWithPreviews />
-      <MilestonesWithUpcomingPayouts />
+    <div className="grid grid cols-2 gap-1">
+      <div className="grid grid-cols-3 gap-1  p-4">
+        <ProjectOverview />
+        <TotalEscrowed />
+        <TotalPayouts />
+        <DeliveredMilestones />
+        <MilestonesWithPreviews />
+        <MilestonesWithUpcomingPayouts />
+      </div>
+      <ProjectList />
     </div>
   );
 }
