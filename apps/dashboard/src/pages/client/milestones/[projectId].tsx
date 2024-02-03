53a2aae31b08ac3bde0db93002c27e11544563e4
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 00:37:19 -0500

    feat: Added client project milestone list page

---
+++ apps/dashboard/src/pages/client/milestones/[projectId].tsx
@@ -0,0 +1,12 @@
+import ClientMilestoneList from "@/components/ClientMilestoneList";
+import React from "react";
+
+function ClientProjectMilestoneListPage() {
+  return (
+    <div>
+      <ClientMilestoneList />
+    </div>
+  );
+}
+
+export default ClientProjectMilestoneListPage;
