356551ad0134cbccb696bffc9c9b3ac5e1d2bcdd
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 00:35:30 -0500

    feat: Added client projects list page

---
+++ apps/dashboard/src/pages/client/projects/index.tsx
@@ -0,0 +1,12 @@
+import ClientProjectList from "@/components/ClientProjectList";
+import React from "react";
+
+function ClientProjectsListPage() {
+  return (
+    <div>
+      <ClientProjectList />
+    </div>
+  );
+}
+
+export default ClientProjectsListPage;
