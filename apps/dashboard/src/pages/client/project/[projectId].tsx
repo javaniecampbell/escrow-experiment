a7b98f6b9692c4894f6f62ac433c9e582e4b9dd2
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 00:37:41 -0500

    feat: Added ClientProjectDetailsPage component

---
+++ apps/dashboard/src/pages/client/project/[projectId].tsx
@@ -0,0 +1,12 @@
+import ClientProjectDetails from "@/components/ClientProjectDetails";
+import React from "react";
+
+function ClientProjectDetailsPage() {
+  return (
+    <div>
+      <ClientProjectDetails />
+    </div>
+  );
+}
+
+export default ClientProjectDetailsPage;
