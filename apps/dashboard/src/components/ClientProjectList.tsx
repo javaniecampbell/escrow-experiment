56f3d1f52c2e52bade501dc97552c170bedf45ee
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-04 14:43:27 -0500

    refactor: ClientProjectList component layout and add MilestoneStepper and ClientDetails components

--- apps/dashboard/src/components/ClientProjectList.tsx
+++ apps/dashboard/src/components/ClientProjectList.tsx
@@ -2,24 +2,51 @@
 import React from "react";
 import useStore from "@/shared/clientStore";
 
+import { MilestoneStepper } from "./MilestoneStepper";
+import { ClientDetails } from "./ClientDetails";
+
 const ClientProjectList = () => {
   const { projects: clientProjects } = useStore();
 
   return (
-    <div className="p-4 border border-gray-300 rounded">
-      <h2 className="text-lg font-semibold mb-4">Your Projects</h2>
+    <div className="">
       {clientProjects.length > 0 ? (
-        <ul className="list-disc pl-6">
+        <div className="grid grid-cols-1 gap-4">
           {clientProjects.map((project) => (
-            <li key={project.id} className="mb-3">
-              <h3 className="text-md font-semibold">
-                {project.name ?? project.title}
-              </h3>
-              <p>Status: {project.status}</p>
-              <p>Balance: ${project.balance.toFixed(2)}</p>
-            </li>
+            <div
+              key={project.id}
+              className="flex justify-between border p-3 rounded"
+            >
+              <div className="flex flex-col w-3/4 gap-2">
+                <h3 className="text-md font-semibold mb-2">
+                  {project.name ?? project.title}
+                </h3>
+                <p className="">{project.description}</p>
+                <div className="flex gap-2 w-full">
+                  <p>Status: {project.status}</p>
+                  <p>Balance: ${project.balance.toFixed(2)}</p>
+                  <p>Total in Escrow: ${project.inEscrow.toFixed(2)}</p>
+                  <p>Total Payouts: ${project.totalPayouts.toFixed(2)}</p>
+                  <p>Number of Milestones: {project.milestones.length}</p>
+                </div>
+                <div className="w-full flex items-center ml-7 mb-8">
+                  <MilestoneStepper />
+                </div>
+              </div>
+              <div className="flex flex-col gap-1 w-1/4">
+                <ClientDetails />
+                <div className="actions flex flex-col gap-1">
+                  <button className="px-4 py-1 mt-2 border border-slate-500 bg-slate-300 text-gray-600 rounded hover:bg-slate-600 hover:text-white">
+                    Contact
+                  </button>
+                  <button className="px-4 py-1 mt-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+                    View Details
+                  </button>
+                </div>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       ) : (
         <p className="text-gray-700">You don't have any active projects.</p>
       )}
