4dc45e43b596b00f52b015191106f65de136df50
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-04 01:16:24 -0500

    refactor: home page layout and styling to allow pointer events

--- apps/dashboard/src/app/site/page.tsx
+++ apps/dashboard/src/app/site/page.tsx
@@ -4,17 +4,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col bg-black text-white">
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col">
         {/* grid */}
 
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] pointer-events-none"></div>
 
-        <h1 className="text-xl font-bold">Welcome to Our Escrow Service</h1>
-        <p>Here's how our service works...</p>
-        {/* <!-- Add mor  e content here --> */}
-        <Link className="text-xl font-bold" href="/service-agreement">
+        <p className="text-center">Here's how our service works...</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
+          <h1 className="text-9xl font-bold text-center">Escrow.</h1>
+        </div>
+        <Link
+          className=" bg-primary text-white px-4 py-2 rounded-md hover:bg-primary/80 "
+          href="/service-agreement"
+        >
           Let's get started
         </Link>
+        {/* <!-- Add mor  e content here --> */}
       </section>
     </>
   );
