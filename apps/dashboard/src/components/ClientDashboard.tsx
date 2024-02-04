6e3e9cdcfe5e786f59c26ae8f0e5792c48c2e564
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-04 14:42:20 -0500

    refactor: ClientDashboard component to have two column layout

--- apps/dashboard/src/components/ClientDashboard.tsx
+++ apps/dashboard/src/components/ClientDashboard.tsx
@@ -1,63 +1,29 @@
 // ClientDashboard.js
 import React from "react";
 import useStore from "@/shared/clientStore";
-import ClientProjectOverview from "./ClientProjectOverview";
+import ClientProjectsOverview from "./ClientProjectOverview";
 import ClientProjects from "./ClientProjects";
 import ClientPayments from "./ClientPayments";
 import ClientNotifications from "./ClientNotifications";
-import { MilestoneStepper } from "./MilestoneStepper";
-import { ClientDetails } from "./ClientDetails";
+import ClientProjectList from "./ClientProjectList";
 
 const ClientDashboard = () => {
   const { projects: clientProjects } = useStore();
 
   return (
-    <div className="p-4 border border-gray-300 rounded">
+    <div className="p-4">
       <h2 className="text-lg font-semibold mb-4">Client Dashboard</h2>
-      <div>
-        {clientProjects.length > 0 ? (
-          <div className="grid grid-cols-1 gap-4">
-            {clientProjects.map((project) => (
-              <div
-                key={project.id}
-                className="flex justify-between border p-3 rounded"
-              >
-                <div className="flex flex-col w-3/4 gap-2">
-                  <h3 className="text-md font-semibold mb-2">
-                    {project.name ?? project.title}
-                  </h3>
-                  <p className="">{project.description}</p>
-                  <div className="flex gap-2 w-full">
-                    <p>Status: {project.status}</p>
-                    <p>Balance: ${project.balance.toFixed(2)}</p>
-                    <p>Total in Escrow: ${project.inEscrow.toFixed(2)}</p>
-                    <p>Total Payouts: ${project.totalPayouts.toFixed(2)}</p>
-                    <p>Number of Milestones: {project.milestones.length}</p>
-                  </div>
-                  <div className="w-full flex items-center ml-7 mb-8">
-                    <MilestoneStepper />
-                  </div>
-                </div>
-                <div className="flex flex-col gap-1 w-1/4">
-                  <ClientDetails />
-                  <div className="actions flex flex-col gap-1">
-                    <button className="px-4 py-1 mt-2 border border-slate-500 bg-slate-300 text-gray-600 rounded hover:bg-slate-600 hover:text-white">
-                      Contact
-                    </button>
-                    <button className="px-4 py-1 mt-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                      View Details
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p className="text-gray-700">You don't have any active projects.</p>
-        )}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-2">
+        <div id="client-project-list" className="col-span-2">
+          <ClientProjectList />
+        </div>
+        <div id="client-overview" className="grid gap-2">
+          <ClientProjectsOverview />
+          <ClientNotifications />
+        </div>
       </div>
 
-      {/* <ClientProjectOverview />
+      {/* 
       <ClientProjects />
       <ClientPayments />
       <ClientNotifications /> */}
