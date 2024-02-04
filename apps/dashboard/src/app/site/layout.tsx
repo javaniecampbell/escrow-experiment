d5df9ff610798b46105d4056cbb98aa9c7e745f4
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 23:52:51 -0500

    refactor: Updated main element width in layout component

--- apps/dashboard/src/app/site/layout.tsx
+++ apps/dashboard/src/app/site/layout.tsx
@@ -2,7 +2,7 @@ import Navigation from "@/components/site/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="h-full">
+    <main className="h-full w-full">
       <Navigation />
       {children}
     </main>
