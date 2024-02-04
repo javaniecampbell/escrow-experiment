fabbbcd6e68535968293d9155c400246a1d45576
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-04 01:56:56 -0500

    feat: Added shadcn ThemeProvider component to layout

--- apps/dashboard/src/pages/_app.tsx
+++ apps/dashboard/src/pages/_app.tsx
@@ -1,7 +1,17 @@
 // These styles apply to every route in the application
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/providers/theme-provider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+  <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        ></ThemeProvider>
+  <Component {...pageProps} />
+  </ThemeProvider>
+        </>
 }
