10c0f9f76fcd6dbf5becae4e8779b37c4c81b951
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-03 23:09:21 -0500

    refactor: moved landing page to site sub folder

--- apps/dashboard/src/app/site/page.tsx
+++ apps/dashboard/src/app/site/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="container mx-auto">
+    <div>
       <h1 className="text-xl font-bold">Welcome to Our Escrow Service</h1>
       <p>Here's how our service works...</p>
       {/* <!-- Add more content here --> */}
       <Link className="text-xl font-bold" href="/service-agreement">
         Let's get started
       </Link>
-    </main>
+    </div>
   );
 }
