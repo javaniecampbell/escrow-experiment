ab12c254d8562a6d3c466d0dec117c43316fc9b0
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-06 08:34:25 -0500

    feat: Added Sidebar component to agency dashboard

--- apps/dashboard/src/pages/agency/dashboard.tsx
+++ apps/dashboard/src/pages/agency/dashboard.tsx
@@ -5,11 +5,13 @@ import ProjectList from "@/components/ProjectList";
 import ProjectOverview from "@/components/ProjectOverview";
 import TotalEscrowed from "@/components/TotalEscrowed";
 import TotalPayouts from "@/components/TotalPayouts";
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 function Dashboard() {
   return (
-    <div className="grid grid cols-2 gap-1">
+    <div className="grid cols-3 gap-1">
+      <Sidebar id="1" type="agency" />
       <div className="grid grid-cols-3 gap-1  p-4">
         <ProjectOverview />
         <TotalEscrowed />
