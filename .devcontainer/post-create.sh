9a7f1ea28494747ccb2882ed0adb43103ed11fd0
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-23 20:33:30 +0000

    fix: removed configuration scripts that prevented container to build

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,20 +13,3 @@ done
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 dapr uninstall # clean up any previous installs
 dapr init -k
-
-# Ensure that the k3d cluster is the current context
-kubectl config current-context
-
-# Initialize rad in the workspace
-rad init
-
-# Verify that the radius-system namespace was created
-
-kubectl get deployments -n radius-system
-
-# view my evironment
-
-rad env list
-
-# Install any monorepo dependencies
-yarn install
