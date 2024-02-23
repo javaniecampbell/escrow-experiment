cbc02b0deda8e1198b34533c42d7edaba7dc65fe
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-02-23 19:31:06 +0000

    chore: update post-create.sh script to include dapr init -k and additional commands
    - ensure rad is configured rather than manually doing so everytime the container iis created

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,6 +12,21 @@ done
 ## Install anything else you need here
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
 dapr uninstall # clean up any previous installs
-dapr init
+dapr init -k
 
-# yarn install
+# Ensure that the k3d cluster is the current context
+kubectl config current-context
+
+# Initialize rad in the workspace
+rad init
+
+# Verify that the radius-system namespace was created
+
+kubectl get deployments -n radius-system
+
+# view my evironment
+
+rad env list
+
+# Install any monorepo dependencies
+yarn install
