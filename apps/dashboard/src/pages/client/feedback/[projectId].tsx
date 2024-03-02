20479794fbe9ff88af52c8013c0461533f77382d
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-02 12:32:36 -0500

    refactor: project feedback page to pass projectId as a prop

--- apps/dashboard/src/pages/client/feedback/[projectId].tsx
+++ apps/dashboard/src/pages/client/feedback/[projectId].tsx
@@ -1,13 +1,28 @@
 import ClientFeedback from "@/components/ClientFeedback";
+import { GetStaticProps } from "next";
 import React from "react";
 
-function ProjectFeedbackPage() {
+function ProjectFeedbackPage({ projectId }: ProjectFeedbackPageProps) {
   return (
     <div>
       <h1 className="">Project Feedback Page</h1>
-      <ClientFeedback />
+      <ClientFeedback projectId={projectId} />
     </div>
   );
 }
+
+type ProjectFeedbackPageProps = {
+  projectId: string;
+};
+export const getStaticProps: GetStaticProps<ProjectFeedbackPageProps> = async ({
+  params,
+}) => {
+  const projectId = params?.projectId as string;
+  return {
+    props: {
+      projectId: projectId,
+    },
+  };
+};
 
 export default ProjectFeedbackPage;
