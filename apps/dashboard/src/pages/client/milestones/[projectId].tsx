6d46993e2c736f58a208e7cc7bcb761f48ccf002
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-02 11:35:00 -0500

    refactor: project milestones page now retrieves projects based on id

--- apps/dashboard/src/pages/client/milestones/[projectId].tsx
+++ apps/dashboard/src/pages/client/milestones/[projectId].tsx
@@ -1,12 +1,44 @@
 import ClientMilestoneList from "@/components/ClientMilestoneList";
+import { GetStaticProps } from "next";
 import React from "react";
 
-function ClientProjectMilestoneListPage() {
+function ClientProjectMilestoneListPage({
+  projectId,
+}: ProjectMilestonesPageProps) {
   return (
     <div>
-      <ClientMilestoneList />
+      <ClientMilestoneList projectId={projectId} />
     </div>
   );
 }
+
+type ProjectMilestonesPageProps = {
+  projectId: string;
+};
+export const getStaticProps: GetStaticProps<
+  ProjectMilestonesPageProps
+> = async ({ params }) => {
+  const projectId = params?.projectId as string;
+  return {
+    props: {
+      projectId: projectId,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const projects = await fetch("http://localhost:3000/api/projects")
+    .then((res) => res.json())
+    .then((data) => data.projects);
+
+  const paths = projects.map((project) => ({
+    params: { projectId: project.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export default ClientProjectMilestoneListPage;
