b78730c186cc409a0b110f0f9286ed6c47c63bf8
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-03-31 20:37:56 -0500

    refactor: BottomSheet component to handle form submission

--- apps/dashboard/src/components/global/bottom-sheet.tsx
+++ apps/dashboard/src/components/global/bottom-sheet.tsx
@@ -12,8 +12,15 @@ import {
 } from "@/components/ui/drawer";
 import { useDialogStore } from "@/shared/dialogStore";
 
-function BottomSheet({ children }: { children?: React.ReactNode }) {
+function BottomSheet({
+  children,
+  targetForm,
+}: {
+  children?: React.ReactNode;
+  targetForm?: string;
+}) {
   const { dialog, toggleIsOpen } = useDialogStore();
+
   return (
     <Drawer
       open={dialog.isOpen}
@@ -21,22 +28,42 @@ function BottomSheet({ children }: { children?: React.ReactNode }) {
       onClose={() => dialog.onCancel()}
     >
       {/* <DrawerTrigger>Open</DrawerTrigger> */}
+
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>{dialog.title}</DrawerTitle>
           <DrawerDescription>{dialog.description}</DrawerDescription>
         </DrawerHeader>
+
         {children}
+
         <DrawerFooter>
           <Button
             onClick={(e: React.SyntheticEvent) => {
               e.preventDefault();
-              dialog.onConfirm();
+              const target = e.target as HTMLButtonElement;
+              const form = document.getElementById(
+                targetForm!
+              ) as HTMLFormElement;
+              console.log("Form:", form);
+              if (form) {
+                if (form.reportValidity()) {
+                  form.submit();
+                  dialog.onConfirm();
+                } else {
+                  return;
+                }
+              } else {
+                dialog.onConfirm();
+              }
             }}
+            type="submit"
+            form={targetForm}
           >
             Submit
           </Button>
-          <DrawerClose>
+
+          <DrawerClose asChild>
             <Button variant="outline">Cancel</Button>
           </DrawerClose>
         </DrawerFooter>
