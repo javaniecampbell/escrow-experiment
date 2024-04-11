90699a1fe7dfdabee4eddf962e11b6ba0930cfe9
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-11 18:13:39 -0500

    feat: added route for accepting in TOS API

---
+++ apps/dashboard/src/app/api/tos/route.ts
@@ -0,0 +1,12 @@
+export const dynamic = 'force-dynamic';
+
+export async function POST(req: Request) {
+    const res = await fetch('http://customer-service/api/terms', { body: JSON.stringify({ "amount": 3.00, "projectId": 1 }), method: 'POST' });
+
+    let data;
+    if (res.ok) {
+        data = await res.json();
+        return Response.redirect(data.url);
+    }
+    Response.json({ error: 'Error creating checkout session' });
+}
