48ea09e2125d26a370909d02dcd01704e1d74425
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-13 10:49:05 -0500

    refactor: payment-service routes to add health check endpoint

--- services/payment-service/routes/index.js
+++ services/payment-service/routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/_health', function (req, res, next) {
+  res.json(200, {
+    status: 'ok'
+  });
 });
 
 module.exports = router;
