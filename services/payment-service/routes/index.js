6d567949a8a6b0116eed4a91bb4d3a545be1ff0b
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-20 15:56:37 -0500

    refactor: payment-service routes to add health check endpoint
    - WIP: payment-service routes to add health check endpoint

--- services/payment-service/routes/index.js
+++ services/payment-service/routes/index.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
-
+const os = require('os')
 /* GET home page. */
 router.get('/_health', function (req, res, next) {
+  // calculates the heap memory used vs maximum
+
+  const memoryUsed = process.memoryUsage().heapUsed; -[0D]
+  const maxMemory = process.memoryUsage().heapTotal;
+
+
+  res.json(200, {
+    status: 'ok'
+  });
+});
+/* GET home page. */
+router.get('/_health/live', function (req, res, next) {
+  res.json(200, {
+    status: 'ok'
+  });
+});
+/* GET home page. */
+router.get('/_health/ready', function (req, res, next) {
   res.json(200, {
     status: 'ok'
   });
