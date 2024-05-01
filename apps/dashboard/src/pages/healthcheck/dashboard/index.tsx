9f6e733ab52798faf403421c6d7f7dd63f16899d
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-30 20:56:47 -0500

    refactor: use ajax for fetching health check data every 5 secs

--- apps/dashboard/src/pages/healthcheck/dashboard/index.tsx
+++ apps/dashboard/src/pages/healthcheck/dashboard/index.tsx
@@ -4,12 +4,40 @@ import React, { useEffect, useState } from "react";
 import { interval } from "rxjs";
 import { map, catchError, switchMap } from "rxjs/operators";
 import { fromFetch } from "rxjs/fetch";
+import { ajax } from "rxjs/ajax";
 
 type Props = {
   healthCheckData?: HealthCheckData | null;
 };
 
-const HealthCheckPage = ({ healthCheckData }: Props) => {
+const HealthCheckPage = ({
+  healthCheckData: initialHealthCheckData,
+}: Props) => {
+  const [healthCheckData, setHealthCheckData] = useState(
+    initialHealthCheckData
+  );
+  useEffect(() => {
+    const subscription = interval(5000)
+      .pipe(
+        switchMap(() => ajax("http://localhost:3000/api/health/v3/spec")),
+        map(
+          (response) => response.response as HealthCheckData | null | undefined
+        ),
+        catchError((error) => {
+          console.error("Error fetching health check data:", error);
+          return [];
+        })
+      )
+      .subscribe({
+        next: (data) => {
+          setHealthCheckData(data);
+        },
+      });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
   return (
     <div>
       <HealthCheckDashboard healthCheckData={healthCheckData} />
@@ -21,7 +49,7 @@ export const getServerSideProps = async () => {
   try {
     const healthCheckData = await new Promise((resolve, reject) => {
       const subscription = fromFetch<HealthCheckData>(
-        "http:localhost:3000/api/health/v3/spec",
+        "http://localhost:3000/api/health/v3/spec",
         {
           selector: (response) => response.json(),
         }
