cfdc6d0ede272e9f7fce0056d45ad42ba571c27b
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-05-01 00:24:48 -0500

    feat: WIP - added stream page but not working yet

---
+++ apps/dashboard/src/pages/healthcheck/dashboard/stream.tsx
@@ -0,0 +1,101 @@
+import HealthCheckDashboard from "@/components/dashboard/HealthCheckDashboard";
+import { HealthCheckData } from "@/shared/app.types";
+import React, { useEffect, useState } from "react";
+import { interval } from "rxjs";
+import { map, catchError, switchMap } from "rxjs/operators";
+import { fromFetch } from "rxjs/fetch";
+import { ajax } from "rxjs/ajax";
+
+type Props = {
+  healthCheckData?: HealthCheckData | null;
+};
+
+const HealthCheckStream = () => {
+  const [healthCheckData, setHealthCheckData] =
+    useState<HealthCheckData | null>(null);
+  const [overallStatus, setOverallStatus] = useState<string | null | undefined>(
+    null
+  );
+
+  useEffect(() => {
+    const eventSource = new EventSource(
+      "http://localhost:3000/api/health/v3/spec/stream"
+    );
+
+    eventSource.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      console.log("Data received:", data);
+      setHealthCheckData(data.data);
+
+      if (event.type === "HEALTHY") {
+        setOverallStatus("pass");
+      } else if (event.type === "UNHEALTHY") {
+        setOverallStatus("fail");
+      } else {
+        setOverallStatus("degraded");
+      }
+    };
+
+    eventSource.addEventListener("error", (event) => {
+      console.error("Error occurred:", event);
+    });
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+  return (
+    <div>
+      <HealthCheckDashboard
+        healthCheckData={healthCheckData}
+        overallStatus={overallStatus}
+      />
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  try {
+    const healthCheckData = await new Promise((resolve, reject) => {
+      const subscription = fromFetch<HealthCheckData>(
+        "http://localhost:3000/api/health/v3/spec",
+        {
+          selector: (response) => response.json(),
+        }
+      )
+        .pipe(
+          map((response) => response),
+          catchError((error) => {
+            reject(error);
+            return [];
+          })
+        )
+        .subscribe({
+          next: (data) => {
+            console.log("Data fetched:", data);
+            resolve(data);
+            subscription.unsubscribe();
+          },
+          error: (error) => {
+            reject(error);
+            subscription.unsubscribe();
+          },
+        });
+    });
+
+    return {
+      props: {
+        healthCheckData,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching health check data:", error);
+    return {
+      props: {
+        healthCheckData: null,
+      },
+    };
+  }
+};
+
+export default HealthCheckStream;
