5323ea2550617233d197ca871b2f112f6d4f5651
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-30 20:41:11 -0500

    refactor: HealthCheckPage component to fetch health check data from server

--- apps/dashboard/src/pages/healthcheck/dashboard/index.tsx
+++ apps/dashboard/src/pages/healthcheck/dashboard/index.tsx
@@ -1,9 +1,12 @@
 import HealthCheckDashboard from "@/components/dashboard/HealthCheckDashboard";
 import { HealthCheckData } from "@/shared/app.types";
 import React, { useEffect, useState } from "react";
+import { interval } from "rxjs";
+import { map, catchError, switchMap } from "rxjs/operators";
+import { fromFetch } from "rxjs/fetch";
 
 type Props = {
-  healthCheckData: HealthCheckData;
+  healthCheckData?: HealthCheckData;
 };
 
 const HealthCheckPage = ({ healthCheckData }: Props) => {
@@ -14,6 +17,51 @@ const HealthCheckPage = ({ healthCheckData }: Props) => {
   );
 };
 
+export const getServerSideProps = async () => {
+  try {
+    const healthCheckData = await new Promise((resolve, reject) => {
+      const subscription = interval(5000)
+        .pipe(
+          switchMap(() =>
+            fromFetch<HealthCheckData>(
+              "http:localhost:3000/api/health/v3/spec",
+              {
+                selector: (response) => response.json(),
+              }
+            )
+          ),
+          map((response) => response),
+          catchError((error) => {
+            reject(error);
+            return [];
+          })
+        )
+        .subscribe({
+          next: (data) => {
+            console.log("Data fetched:", data);
+            resolve(data);
+            subscription.unsubscribe();
+          },
+          error: (error) => {
+            reject(error);
+            subscription.unsubscribe();
+          },
+        });
+    });
 
+    return {
+      props: {
+        healthCheckData,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching health check data:", error);
+    return {
+      props: {
+        healthCheckData: null,
+      },
+    };
+  }
+};
 
 export default HealthCheckPage;
