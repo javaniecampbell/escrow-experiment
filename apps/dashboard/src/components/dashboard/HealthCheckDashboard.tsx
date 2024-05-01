74c19f02f67c960842889a687bb8eb5acb2253bc
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-30 19:59:59 -0500

    refactor: component to use HealthCheckDashboardProps

--- apps/dashboard/src/components/dashboard/HealthCheckDashboard.tsx
+++ apps/dashboard/src/components/dashboard/HealthCheckDashboard.tsx
@@ -1,41 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { map, catchError, switchMap } from "rxjs/operators";
-import { ajax } from "rxjs/ajax";
-import { of, interval } from "rxjs";
+import React from "react";
 import moment from "moment";
 
-const HealthCheckDashboard = () => {
-  const [healthCheckData, setHealthCheckData] = useState<any>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+type HealthCheckData = {
+  checks: any;
+};
 
-  useEffect(() => {
-    const subscription = interval(5000) // Emit a value every 5 seconds
-      .pipe(
-        switchMap(() => ajax("http://localhost:3000/api/health/v3/spec")),
-        map((response) => response.response),
-        catchError((error) => {
-          setError(error.message);
-          return of(null);
-        })
-      )
-      .subscribe({
-        next: (data) => {
-          setHealthCheckData(data as any);
-          setLoading(false);
-          setError(null);
-        },
-        error: (error) => {
-          setError(error.message);
-          setLoading(false);
-        },
-      });
+type HealthCheckDashboardProps = {
+  healthCheckData?: HealthCheckData;
+  loading?: boolean;
+  error?: string | null;
+};
 
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
-
+const HealthCheckDashboard = ({
+  healthCheckData,
+  loading,
+  error,
+}: HealthCheckDashboardProps) => {
   const renderHealthCheckCard = (checkDetails: any) => {
     const { componentName, measurementName, details } = checkDetails;
     const title = `${componentName}${
