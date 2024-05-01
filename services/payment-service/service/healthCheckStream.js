0f2e01510b95dd0febc5b4386e1f780c295f04e8
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-04-30 21:09:35 -0500

    feat: health check stream to use HealthCheckContext and strategies

---
+++ services/payment-service/service/healthCheckStream.js
@@ -0,0 +1,33 @@
+const { interval, Observable } = require('rxjs');
+const { map, concatMap } = require('rxjs/operators');
+// const { performHealthChecks } = require('./healthChecks');
+const { HealthCheckContext } = require('@ddaw/healthcheck-sdk');
+const {
+    CacheHealthCheckStrategy,
+    DatabaseHealthCheckStrategy,
+    ThirdPartyHealthCheckStrategy
+} = require('../healthchecks');
+
+// Create a HealthCheckContext instance
+const healthCheckContext = new HealthCheckContext();
+
+// Add health check strategies
+healthCheckContext.addStrategy(new DatabaseHealthCheckStrategy());
+healthCheckContext.addStrategy(new CacheHealthCheckStrategy());
+healthCheckContext.addStrategy(new ThirdPartyHealthCheckStrategy());
+
+const healthCheckStream = Observable.create((observer) => {
+    const intervalSubscription = interval(5000)
+        .pipe(
+            concatMap(() => healthCheckContext.performHealthCheck()),
+            map((healthCheckData) => observer.next(healthCheckData))
+        )
+        .subscribe(
+            null,
+            (error) => observer.error(error)
+        );
+
+    return () => intervalSubscription.unsubscribe();
+});
+
+module.exports = healthCheckStream;
