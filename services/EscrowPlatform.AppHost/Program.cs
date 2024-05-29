85abbe21cd7bdf48831db42c6e02eab3fbe6f468
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-05-29 18:14:49 -0500

    feat: added paymentservice & dashboard to aspire orchestration

--- services/EscrowPlatform.AppHost/Program.cs
+++ services/EscrowPlatform.AppHost/Program.cs
@@ -1,5 +1,22 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.NotificationService_Api>("notificationservice-api");
+var database = builder.AddPostgres("escrowdb");
+
+var notificationServiceApi = builder.AddProject<Projects.NotificationService_Api>("notificationservice-api");
+
+var paymentServiceApi = builder.AddNpmApp("paymentservice-api","../payment-service", "dev")
+	.WithReference(notificationServiceApi)
+	.WithReference(database)
+	.WithHttpEndpoint(env: "PORT")
+	.WithExternalHttpEndpoints()
+	.PublishAsDockerFile();
+
+var dashboardFrontend = builder.AddNpmApp("dashboard-nextjs", "../../apps/dashboard", "dev")
+	.WithReference(notificationServiceApi)
+	.WithHttpEndpoint(env: "PORT")
+	.WithExternalHttpEndpoints()
+	.PublishAsDockerFile();
+
+
 
 builder.Build().Run();
