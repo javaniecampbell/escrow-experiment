755c59fb8aeffd1b716296afb947a34380c50a00
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-05-29 18:23:41 -0500

    feat: added client project to aspire orchestration

--- services/EscrowPlatform.AppHost/Program.cs
+++ services/EscrowPlatform.AppHost/Program.cs
@@ -5,13 +5,21 @@ var database = builder.AddPostgres("escrowdb");
 var notificationServiceApi = builder.AddProject<Projects.NotificationService_Api>("notificationservice-api");
 
 var paymentServiceApi = builder.AddNpmApp("paymentservice-api","../payment-service", "dev")
-	.WithReference(notificationServiceApi)
 	.WithReference(database)
+	.WithReference(notificationServiceApi)
 	.WithHttpEndpoint(env: "PORT")
 	.WithExternalHttpEndpoints()
 	.PublishAsDockerFile();
 
-var dashboardFrontend = builder.AddNpmApp("dashboard-nextjs", "../../apps/dashboard", "dev")
+builder.AddNpmApp("dashboard-nextjs", "../../apps/dashboard", "dev")
+	.WithReference(paymentServiceApi)
+	.WithReference(notificationServiceApi)
+	.WithHttpEndpoint(env: "PORT")
+	.WithExternalHttpEndpoints()
+	.PublishAsDockerFile();
+
+builder.AddNpmApp("clientportal-nextjs", "../../apps/client-portal", "dev")
+	.WithReference(paymentServiceApi)
 	.WithReference(notificationServiceApi)
 	.WithHttpEndpoint(env: "PORT")
 	.WithExternalHttpEndpoints()
