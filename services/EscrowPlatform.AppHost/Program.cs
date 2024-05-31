e74f6cfaab00cc6ebc06d45d49d7ee4d07cf4c3c
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-05-31 12:29:16 -0500

    fix: missing dependencies for payment-service

--- services/EscrowPlatform.AppHost/Program.cs
+++ services/EscrowPlatform.AppHost/Program.cs
@@ -1,28 +1,42 @@
+using Aspire.Hosting;
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var database = builder.AddPostgres("escrowdb");
+var storage = builder.AddAzureStorage("escrowstorage");
+
+if (builder.Environment.IsDevelopment())
+{
+	storage.RunAsEmulator();
+}
+
+var blobStorage = storage.AddBlobs("escrowblobstorage");
 
 var notificationServiceApi = builder.AddProject<Projects.NotificationService_Api>("notificationservice-api");
 
 var paymentServiceApi = builder.AddNpmApp("paymentservice-api","../payment-service", "dev")
-	.WithReference(database)
-	.WithReference(notificationServiceApi)
 	.WithHttpEndpoint(env: "PORT")
 	.WithExternalHttpEndpoints()
+	.WithReference(database)
+	.WithEnvironment("DATABSE_URL", database)
+	.WithReference(notificationServiceApi)
+	// How to add environment variables in .dotnet aspire
+	.WithEnvironment("AZURE_STORAGE_CONNECTION_STRING",blobStorage)
 	.PublishAsDockerFile();
 
 builder.AddNpmApp("dashboard-nextjs", "../../apps/dashboard", "dev")
-	.WithReference(paymentServiceApi)
-	.WithReference(notificationServiceApi)
 	.WithHttpEndpoint(env: "PORT")
 	.WithExternalHttpEndpoints()
+	.WithReference(paymentServiceApi)
+	.WithReference(notificationServiceApi)
 	.PublishAsDockerFile();
 
 builder.AddNpmApp("clientportal-nextjs", "../../apps/client-portal", "dev")
-	.WithReference(paymentServiceApi)
-	.WithReference(notificationServiceApi)
 	.WithHttpEndpoint(env: "PORT")
 	.WithExternalHttpEndpoints()
+	.WithReference(paymentServiceApi)
+	.WithReference(notificationServiceApi)
 	.PublishAsDockerFile();
 
 
