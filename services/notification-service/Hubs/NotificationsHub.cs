b4c8cefd9d95e9bdd93560d4b1a077e75cc9c688
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-08 17:34:34 -0500

    refactor: added method to send notification to user

--- services/notification-service/Hubs/NotificationsHub.cs
+++ services/notification-service/Hubs/NotificationsHub.cs
@@ -4,5 +4,16 @@ namespace NotificationService.Api.Hubs
 {
 	public class NotificationsHub : Hub
 	{
+		/// <summary>
+		/// Send a notification to a specific user
+		/// </summary>
+		/// <param name="userId">The user we want tp receive the notification</param>
+		/// <param name="message">the body of the notification</param>
+		/// <returns></returns>
+		public async Task SendNotification(string userId, string message)
+		{
+			// This method is called from the client to send a notification
+			await Clients.User(userId).SendAsync("ReceiveNotification", message);
+		}
 	}
 }
