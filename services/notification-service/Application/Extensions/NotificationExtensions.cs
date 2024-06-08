4c25a2f153b1904ef1367e91a52b707fb470280f
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-08 17:07:22 -0500

    feat: added notification extension methods

---
+++ services/notification-service/Application/Extensions/NotificationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using NotificationService.Api.Domain.Entities;
+
+namespace NotificationService.Api.Application.Extensions
+{
+	public static class NotificationExtensions
+	{
+		public static Notification MarkAsRead(this Notification notification)
+		{
+			notification.IsRead = true;
+			return notification;
+		}
+		// Add more notification-related extensions as needed
+	}
+}
