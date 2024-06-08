09684a1b322a6211a165f9b739f598985a0d013e
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-08 17:12:46 -0500

    feat: added notification channels model

---
+++ services/notification-service/Application/Models/NotificationDeliveryChannels.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificationService.Api.Application.Models
+{
+	public enum NotificationDeliveryChannels
+	{
+		RealTime,
+		Email,
+		SMS,
+		// Add more delivery channels as needed
+	}
+}
