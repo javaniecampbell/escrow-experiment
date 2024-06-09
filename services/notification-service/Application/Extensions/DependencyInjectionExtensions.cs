b7ae642ab8eceeff7dd8cc6dfef887c405622ff2
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-09 11:45:37 -0500

    refactor: added service collection extensions for each layer
    - for infrastructure layers
    - for application layer

---
+++ services/notification-service/Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Notifications.Api.Application.Extensions
+{
+	public static class DependencyInjectionExtensions
+	{
+		public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
+		{
+			//services.AddScoped<INotificationService, NotificationService>();
+
+			return services;
+		}
+	}
+}
---
+++ services/notification-service/Infrastructure/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Notifications.Api.Infrastructure.Extensions
+{
+	public static class DependencyInjectionExtensions
+	{
+		public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+		{
+			//services.AddDbContext<NotificationDbContext>(options =>
+			//	options.UseSqlServer(configuration.GetConnectionString("NotificationDbConnection")));
+
+			//services.AddScoped<INotificationRepository, NotificationRepository>();
+
+			return services;
+		}
+	}
+}
