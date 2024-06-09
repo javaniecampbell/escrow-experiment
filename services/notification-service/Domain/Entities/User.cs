930fe4d490717ea7609e90f138a6d2990ad44277
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-06-09 11:02:53 -0500

    feat: added relationship to Notifications

--- services/notification-service/Domain/Entities/User.cs
+++ services/notification-service/Domain/Entities/User.cs
@@ -5,6 +5,8 @@
 		public string Id { get; set; }
 		public string Email { get; set; }
 		public NotificationSetting NotificationSetting { get; set; }
+
+		public virtual ICollection<Notification> Notifications { get; set; }
 	}
 
 }
