7c4c5f747f7cc9595ea396f96a7d00920459ef58
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-09-01 16:07:26 -0400

    feat: Adds 3 nottification types

---
+++ .scripts/migrations/V0.05__Inserts_3_NotificationTypes.sql
@@ -0,0 +1,6 @@
+INSERT INTO NotificationTypes (Name, Description)
+VALUES
+    ('ProjectUpdate', 'Notification for project updates'),
+    ('BillingReminder', 'Notification for billing reminders'),
+    ('MessageReceived', 'Notification for received messages'),
+;
