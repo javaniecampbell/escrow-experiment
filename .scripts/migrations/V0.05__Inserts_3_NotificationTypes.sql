f1a9414b9226a36a27b9dcf75c97eb531d3dd986
Javanie Campbell <1676496+javaniecampbell@users.noreply.github.com>
2024-09-01 17:11:26 -0400

    refactor: Adds 4 more notification types to the notification type enum

--- .scripts/migrations/V0.05__Inserts_3_NotificationTypes.sql
+++ .scripts/migrations/V0.05__Inserts_3_NotificationTypes.sql
@@ -1,4 +1,4 @@
-INSERT INTO NotificationTypes (Name, Description)
+INSERT INTO NotificationTypes (TypeName, TypeDescription)
 VALUES
     ('ProjectUpdate', 'Notification for project updates'),
     ('BillingReminder', 'Notification for billing reminders'),
---
+++ .scripts/migrations/V0.06__Inserts_4_NotificationTypes.sql
@@ -0,0 +1,7 @@
+INSERT INTO NotificationTypes (TypeName, TypeDescription)
+VALUES
+    ('MilestoneCompleted', 'Notification for completed milestones'),
+    ('NewClientMessage', 'Notification for new client messages'),
+    ('NewProjectAssigned', 'Notification for new project assignments'),
+    ('MilestoneDue', 'Notification for upcoming milestone due dates'),
+;
